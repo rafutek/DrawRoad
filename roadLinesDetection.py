a90c729d79d8e10e9a70fc0404fd90e40836166a
raph <raphaelroyperso@gmaill.com>
2019-07-17 00:13:49 +0200

    better angle and comments

--- roadLinesDetection.py
+++ roadLinesDetection.py
@@ -13,7 +13,9 @@ import matplotlib.pyplot as plt
 
 
 ############## variables definition ##############
-window_name = 'Line Detection'
+window_original_road = 'Lines Detection'
+window_drawn_road = 'Road Drawing'
+
 
 delay = 200
 
@@ -51,13 +53,13 @@ line_length = 5
 last_x = int(img.shape[1]/2)
 last_y = int(img.shape[0])
 
-theta_straight_line = 1.39
+theta_straight_line = 1.45
 degree_straight_line = -90
 coef = degree_straight_line/theta_straight_line
 
 ############## functions definition ##############
 
-
+# draw the line (rho, theta) in img with color
 def drawLine(rho, theta, img, color):
     a = np.cos(theta)
     b = np.sin(theta)
@@ -69,55 +71,57 @@ def drawLine(rho, theta, img, color):
     y2 = int(y0 - 1000*(a))
     cv2.line(img,(x1,y1),(x2,y2),color,2)
 
-
-#img = np.append(new_array, img, axis=1)
-
+# computes and return the next line point with the first point, the angle and the length of the line
 def nextLinePoint(x1,y1, angle, length):
     x2 =  int(round(x1 + length * np.cos(angle * np.pi / 180.0)))
     y2 =  int(round(y1 + length * np.sin(angle * np.pi / 180.0)))
     return x2, y2
 
+# create an array of zeros of the size passed in parameter
 def createArray(height, width, depth):
     size = height, width, depth
     return np.zeros(size, dtype=np.uint8)
 
+# creates a new array/image if the point (x2,y2) is not in the array img 
+# this new array/image contain the previous img at the right place (below, to the left, etc..) in order to have (x2,y2) at a border
+# and change the values of the points (x1,y1) and (x2,y2)
 def newArray(x1, y1, x2, y2, img):
-    space = 0
     new_array = img
 
-    if x2 < 0:
-        space = abs(x2)
-        space_array = createArray(img.shape[0], space, 1)
-        new_array = np.append(space_array, img, axis=1)           
-        x2 += space
-        x1 += space
+    if not nextPointDrawable(x2,y2,img):
+        if x2 < 0:
+            space = abs(x2)
+            space_array = createArray(img.shape[0], space, 1)
+            new_array = np.append(space_array, img, axis=1)           
+            x2 += space
+            x1 += space
 
-    elif x2 > img.shape[1]:
-        space = x2 - img.shape[1]
-        space_array = createArray(img.shape[0], space, 1)
-        new_array = np.append(img, space_array, axis=1)
+        elif x2 > img.shape[1]:
+            space = x2 - img.shape[1]
+            space_array = createArray(img.shape[0], space, 1)
+            new_array = np.append(img, space_array, axis=1)
 
-    
-    if y2 < 0:
-        space = abs(y2)
-        space_array = createArray(space, img.shape[1], 1)
-        new_array = np.append(space_array, img, axis=0)           
-        y2 += space
-        y1 += space
-    elif y2 > img.shape[0]:
-        space = y2 - img.shape[0]
-        space_array = createArray(space, img.shape[1], 1)
-        new_array = np.append(img, space_array, axis=0)   
+        
+        if y2 < 0:
+            space = abs(y2)
+            space_array = createArray(space, img.shape[1], 1)
+            new_array = np.append(space_array, img, axis=0)           
+            y2 += space
+            y1 += space
+        elif y2 > img.shape[0]:
+            space = y2 - img.shape[0]
+            space_array = createArray(space, img.shape[1], 1)
+            new_array = np.append(img, space_array, axis=0)   
 
     
     return new_array, x1, y1, x2, y2
 
-
+# say if the point (x2,y2) is contained the array/image or not
 def nextPointDrawable(x2,y2, img):
     return x2 >= 0 and x2 < img.shape[1] and y2 >= 0 and y2 < img.shape[0]
 
-
-def lineAnalysis(rho, theta, img):
+# analyse the line passed in parameter to say if it's a left line, a right line, or none of them
+def lineAnalysis(rho, theta):
     left_line = False
     right_line = False 
 
@@ -129,8 +133,8 @@ def lineAnalysis(rho, theta, img):
     return left_line, right_line
 
 
-# 'ESC' to return false, 'SPACE' to next frame, any other to next line
-def linesLeftAndRight(lines,window, img):
+# take every line detected in parameter, and find one left line and one right line
+def linesLeftAndRight(lines):
     left_rho = left_theta = 0
     right_rho = right_theta = 0
     left_line_found = right_line_found = False
@@ -138,7 +142,7 @@ def linesLeftAndRight(lines,window, img):
     if lines is not None:
         for line in lines:
             rho, theta = line[0]
-            left_line, right_line = lineAnalysis(rho, theta, img)
+            left_line, right_line = lineAnalysis(rho, theta)
             
             if left_line and not left_line_found:
                 left_line_found = True
@@ -155,8 +159,11 @@ def linesLeftAndRight(lines,window, img):
     return left_rho, left_theta, right_rho, right_theta
 
 
+# return the angle in degree of the line to draw
 def thetaToDegree(theta):
     return coef * theta
+
+
 
 ############## main program ##############
 
@@ -168,12 +175,11 @@ if not video.isOpened():
 
 while(video.isOpened()):
     read_flag, frame = video.read()
-    # if the video is finished, we play it again
+    # if the video is finished, we get out of the loop
     if not read_flag:
-        print("play video again")
-        video = cv2.VideoCapture(videoName)
-        continue
-    # we only want the lower part of the road
+        print(videoName+" finished")
+        break
+    # we only want the lower part of the image: the road
     height, width = frame.shape[:2]
     img_road = frame[height-80:height, 0:width]
 
@@ -194,9 +200,9 @@ while(video.isOpened()):
     lines = cv2.HoughLines(edges, 1, np.pi / 180, hThreshold)
 
     # find left line and right line
-    left_rho, left_theta, right_rho, right_theta = linesLeftAndRight(lines, window_name, img_road)
+    left_rho, left_theta, right_rho, right_theta = linesLeftAndRight(lines)
 
-    #draw
+    # draw
     if left_rho != 0 and left_theta != 0 and right_rho != 0 and right_theta != 0:
         drawLine(left_rho, left_theta, img_road, (0,0,255))
         drawLine(right_rho, right_theta, img_road, (255,0,0))
@@ -204,17 +210,18 @@ while(video.isOpened()):
         theta_mean_line = left_theta - right_theta 
         x2, y2 = nextLinePoint(last_x, last_y, thetaToDegree(theta_mean_line), line_length)
         
-        if not nextPointDrawable(x2,y2,img):
-            # increase img to be able to draw the line completly
-            img, last_x, last_y, x2, y2  = newArray(last_x,last_y,x2,y2,img)
+        # if necessary, increase img to be able to draw the line completly
+        img, last_x, last_y, x2, y2  = newArray(last_x,last_y,x2,y2,img)
 
 
-    #display
-    cv2.imshow(window_name, img_road)
+    # display
+    cv2.imshow(window_original_road, img_road)
     if cv2.waitKey(delay) == 27:
         break
     cv2.line(img,(last_x,last_y),(x2,y2),(255,255,255),2)
-    cv2.imshow("road", img)
+    cv2.imshow(window_drawn_road, img)
+
+    # new point becomes last point at each iteration
     last_x = x2
     last_y = y2
     
